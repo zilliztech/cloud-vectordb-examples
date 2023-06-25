76fd5216a44ea364433d7f3c2f569d31526c0037
ryjiang <ruiyi.jiang@zilliz.com>
2023-06-25 17:32:21 +0800

    Update config.js

--- node/config.js
+++ node/config.js
@@ -2,5 +2,5 @@ export const config = {
   uri: "https://in03-XXXXXXXXXXXXX.api.gcp-us-west1.cloud-uat3.zilliz.com:port",
   user: 'username',
   passsword: 'password',
-  token: 'some-token', // please check your Zilliz Cloud connection guide
+  token: 'some-token', // optinal, please check your Zilliz Cloud connection guide
 };
