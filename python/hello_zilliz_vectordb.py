79763482f0c4efc1fef3f7a6b8a4f0e2f41b31fd
huangjincheng2022 <jincheng.huang@zilliz.com>
2024-03-28 18:15:36 +0800

    Update hello_zilliz_vectordb.py

--- python/hello_zilliz_vectordb.py
+++ python/hello_zilliz_vectordb.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     # insert data with customized ids
     nb = 10000
-    insert_rounds = 300
+    insert_rounds = 500
     start = 0  # first primary key id
     total_rt = 0  # total response time for inert
     print(f"Inserting {nb * insert_rounds} entities... ")
